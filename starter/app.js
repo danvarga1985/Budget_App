3098b5d6c4003414d5d9ba62918efb5409e3c402
danvarga1985 <danvarga1985@gmail.com>
2020-05-15 10:15:31 +0200

    Added event listener for the 'add' button and the 'enter key'

--- starter/app.js
+++ starter/app.js
@@ -1,28 +1,43 @@
+//Budget controller
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return  {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    //code
 
 })();
 
+
+//UI controller
 var UIController = (function() {
     //code
 })();
 
+
+//Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(10);
-    
-    return {
-        anotherPublicMethod: function() {
-            console.log(z);
+
+    var ctrlAddItem= function() {
+         //1. Get the field input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the new item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+       ctrlAddItem();
+
+    });
+
+    document.addEventListener('keypress', function(event) {
+        //Check if Enter key was pressed - Enter keycode: 13 ** event.which - used for older browsers
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
-    }
+    });
+
 })(budgetController, UIController);
+
